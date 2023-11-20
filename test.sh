aa22431a4bde5a9f510348f21bb8625066555048
Max Glazkov <maksimglaz@gmail.com>
2023-11-20 22:03:02 +0200

    update

--- test.sh
+++ test.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 source ./test.env
-echo "1 $MYVAR2 2"
-echo "1 $MYVAR3 2"
+# Проверяем, что передан аргумент
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <variable_name>"
+  exit 1
+fi
+
+# Используем первый аргумент как имя переменной
+variable_name=$1
+
+# Получаем значение переменной
+variable_value="${!variable_name}"
+
+# Выводим значение переменной
+echo "Значение переменной $variable_name: $variable_value"
